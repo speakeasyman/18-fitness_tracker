1040ced036f5c691bcc854f7aedfdfa12352072f
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-08-08 18:30:16 -0400

    another route made

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,21 @@ router.get('/api/workouts', (req, res) => {
     .catch(err => { res.json(err); })
 });
 
+router.post('/api/workouts', ( {body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => { res.json(dbWorkout) } )
+    .catch(err => {res.json(err) } );
+});
 
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, 
+        { $push: { exercises: req.body } }, 
+        { new: true, runValidators: true }
+    )
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); });
+});
 
 
 
