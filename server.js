704ba1764736ad72df9fa09fad3f5ec9b106714b
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-08-07 21:37:30 -0400

    created server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+// const mongojs = require("mongojs");
+const mongoose  = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
+    }
+    )
+
+
+
+    app.listen(PORT, () => {
+        console.log(`App running on port ${PORT}!`);
+        });
+
+    app.use(require('./public/api'))
